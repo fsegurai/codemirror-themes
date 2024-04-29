7ec66a84fad53c217a338fe4179a5a69e84a1d10
fsegurai <fsegurai@ucenfotec.ac.cr>
2024-04-28 22:11:19 -0600

    upg/0.0.8 - feat/material-dark theme

--- example/themes.ts
+++ example/themes.ts
@@ -2,6 +2,7 @@ import { basicDark } from '../packages/basic-dark/src'
 import { basicLight } from '../packages/basic-light/src'
 import { gruvboxDark } from '../packages/gruvbox-dark/src'
 import { gruvboxLight } from '../packages/gruvbox-light/src'
+import { materialDark } from '../packages/material-dark/src'
 import { solarizedDark } from '../packages/solarized-dark'
 import { solarizedLight } from '../packages/solarized-light/src'
 
@@ -21,6 +22,10 @@ const themes = [
   {
     name: 'gruvbox-light',
     extension: gruvboxLight
+  },
+  {
+    name: 'material-dark',
+    extension: materialDark
   },
   {
     name: 'solarized-dark',
