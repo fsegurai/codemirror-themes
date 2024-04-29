ecd5c304cb9134ede9994551c737931c86591670
fsegurai <fsegurai@ucenfotec.ac.cr>
2024-04-28 21:40:07 -0600

    upg/0.0.5 feat/solarized-light theme

--- example/themes.ts
+++ example/themes.ts
@@ -1,9 +1,14 @@
 import { solarizedDark } from '../packages/solarized-dark'
+import { solarizedLight } from '../packages/solarized-light/src'
 
 const themes = [
   {
     name: 'solarized-dark',
     extension: solarizedDark
+  },
+  {
+    name: 'solarized-light',
+    extension: solarizedLight
   }
 ]
 
