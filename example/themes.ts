b0eb029b58b94ea5e715201cfe6e1694dfd7b912
fsegurai <fsegurai@ucenfotec.ac.cr>
2024-04-28 21:52:30 -0600

    upg/0.0.6 - feat/basic-dark & basic-light themes

--- example/themes.ts
+++ example/themes.ts
@@ -1,7 +1,17 @@
+import { basicDark } from '../packages/basic-dark/src'
+import { basicLight } from '../packages/basic-light/src'
 import { solarizedDark } from '../packages/solarized-dark'
 import { solarizedLight } from '../packages/solarized-light/src'
 
 const themes = [
+  {
+    name: 'basic-dark',
+    extension: basicDark
+  },
+  {
+    name: 'basic-light',
+    extension: basicLight
+  },
   {
     name: 'solarized-dark',
     extension: solarizedDark
