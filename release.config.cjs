b77c1ce5dc86b1e7544a0cccb2c5c4c55bfb1521
fsegurai <fabiansegu@gmail.com>
2025-02-03 22:04:06 -0600

    fix: release workflow

--- release.config.cjs
+++ release.config.cjs
@@ -5,5 +5,10 @@ module.exports = {
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog', // This will generate the changelog file
     '@semantic-release/git', // This will commit the changelog back to the repo
+    {
+      path: '@semantic-release/github',
+      verifyConditions: false, // Skip actual release creation to avoid pushing new tags
+      publish: false, // Skip the publishing of the release
+    },
   ],
 };
