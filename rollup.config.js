73459e6efbb20603e6e756d911d1bfbd241b6e9b
fsegurai <fabiansegu@gmail.com>
2025-05-21 23:00:59 -0600

    core: added utils and tsconfig rollup alias

--- rollup.config.js
+++ rollup.config.js
@@ -2,13 +2,17 @@ import typescript from '@rollup/plugin-typescript';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
+import alias from '@rollup/plugin-alias';
 import dev from 'rollup-plugin-dev';
 import process from 'process';
+import { fileURLToPath } from 'url';
+import path from 'path';
 import dotenv from 'dotenv';
 dotenv.config();
 
 const { NODE_ENV, HOST_URL } = process.env;
 const OUTPUT_DIR = './demo/dist';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default {
   input: [
@@ -25,6 +29,12 @@ export default {
   ],
   external: [],
   plugins: [
+    alias({
+      entries: [
+        { find: '@utils', replacement: path.resolve(__dirname, 'packages/.helper/utils.ts') },
+        { find: '@cmt', replacement: path.resolve(__dirname, 'packages') },
+      ],
+    }),
     typescript({
       tsconfig: './tsconfig.json',
       compilerOptions: {
