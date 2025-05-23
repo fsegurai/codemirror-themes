2b113aab0699b7e56d1c77015f47e8286e386424
fsegurai <fabiansegu@gmail.com>
2025-05-22 19:18:24 -0600

    feat: new standard properties

--- packages/.helper/utils.ts
+++ packages/.helper/utils.ts
@@ -12,6 +12,33 @@ export const generalGutter = {
   border: 'none',
   paddingRight: '8px',
   fontSize: '0.9em',
+  fontWeight: '500',
+};
+
+export const generalPanel = {
+  border: 'none',
+  borderRadius: '4px',
+  padding: '2px 10px',
+};
+
+export const generalLine = {
+  borderRadius: '2px',
+};
+
+export const generalMatching = {
+  borderRadius: '2px',
+};
+
+export const generalPlaceholder = {
+  borderRadius: '4px',
+  padding: '0 5px',
+  margin: '0 2px',
+};
+
+export const generalScroller = {
+  width: '12px',
+  height: '12px',
+  borderRadius: '6px',
 };
 
 export const generalSearchField = {
@@ -21,5 +48,8 @@ export const generalSearchField = {
 
 export const generalTooltip = {
   borderRadius: '4px',
+  borderRadiusSelected: '3px',
+  lineHeight: '1.3',
   padding: '4px 8px',
+  paddingRight: '8px',
 };
