949d415c705ecc30d3489f5341c75e3c440d6fe3
fsegurai <fabiansegu@gmail.com>
2025-06-02 22:28:35 -0600

    fix: improved utils declaration on bundle

--- rollup.config.js
+++ rollup.config.js
@@ -3,12 +3,62 @@ import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
 import dev from 'rollup-plugin-dev';
+import fs from 'fs';
+import path from 'path';
 import process from 'process';
 import dotenv from 'dotenv';
 dotenv.config();
 
 const { NODE_ENV, HOST_URL } = process.env;
 const OUTPUT_DIR = './demo/dist';
+
+/**
+ * Copies utils.ts from the helper package to each theme's src directory.
+ * @returns { name: string, buildStart: Function, watchChange: Function }
+ */
+function copyUtilsToThemes() {
+  let initialBuild = true;
+  const utilsPath = path.resolve('./packages/.helper/utils.ts');
+  
+  return {
+    name: 'copy-utils',
+    buildStart() {
+      // Copy utils on initial build or when utils.ts changes
+      if (initialBuild) {
+        copyUtils();
+        initialBuild = false;
+      }
+      
+      // Add the utils file to the watch list
+      this.addWatchFile(utilsPath);
+    },
+    // Handle file changes during watch mode
+    watchChange(id) {
+      if (id === utilsPath) {
+        copyUtils();
+      }
+    },
+  };
+  
+  /**
+   * Copies the utils.ts file to each theme's src directory.
+   * This function reads the utils.ts file from the helper package and writes it
+   */
+  function copyUtils() {
+    const utilsContent = fs.readFileSync(utilsPath, 'utf8');
+    const packagesDir = path.resolve('./packages');
+    const themeDirs = fs.readdirSync(packagesDir)
+      .filter(dir => dir !== '.helper' && dir !== 'bundle')
+      .map(dir => path.join(packagesDir, dir));
+    
+    themeDirs.forEach(dir => {
+      const srcDir = path.join(dir, 'src');
+      if (fs.existsSync(srcDir)) {
+        fs.writeFileSync(path.join(srcDir, 'utils.ts'), utilsContent);
+      }
+    });
+  }
+}
 
 export default {
   input: [
@@ -25,6 +75,7 @@ export default {
   ],
   external: [],
   plugins: [
+    copyUtilsToThemes(),
     typescript({
       tsconfig: './tsconfig.json',
       compilerOptions: {
